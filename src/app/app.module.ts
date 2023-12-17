5bc9dc65bd28335da2589c4c49a6dafaa6e12b8b
haroune <haroune.boumedjirek2@gmail.com>
2023-12-17 21:15:18 +0100

    adding block & envireneme,ts & user-profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { EnvironmentsComponent } from './environments/environments.component';
+import { BlockComponent } from './block/block.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    EnvironmentsComponent,
+    BlockComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+
+  user = {
+    name: 'Doe',
+    firstName: 'John',
+    age: 25,
+    quote: '',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+
+  hideAge = false;
+
+  toggleAgeVisibility() {
+    this.hideAge = !this.hideAge;
+  }
+
+}
