a1465a260d7c972503793c24341ad15eb5b560c1
boumedjirek <haroun902009@hotmail.fr>
2023-12-21 10:02:45 +0100

    binding challenge

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '../user.model';  // Assurez-vous d'ajuster le chemin
+
 
 @Component({
   selector: 'app-user-profile',
@@ -7,18 +9,17 @@ import { Component } from '@angular/core';
 })
 export class UserProfileComponent {
 
-  user = {
-    name: 'Doe',
-    firstName: 'John',
-    age: 25,
-    quote: '',
-    photo: 'https://randomuser.me/api/portraits/lego/2.jpg'
-  };
+  //Bonus POO : créer une classe User qui te permet d'instancier ton objet user grâce au mot clef new depuis ton composant user-profile.
+  user = new User('Doe', 'John', 25, '', 'https://randomuser.me/api/portraits/lego/2.jpg');
 
   hideAge = false;
 
   toggleAgeVisibility() {
     this.hideAge = !this.hideAge;
   }
+
+    // Dans UserProfileComponent
+  
+  
 
 }
