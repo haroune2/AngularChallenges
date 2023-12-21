45193f4e7c8a03508f6f9a78a4f05456372e196c
boumedjirek <haroun902009@hotmail.fr>
2023-12-21 10:55:56 +0100

    binding challenge

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -10,6 +10,8 @@ import { User } from '../user.model';  // Assurez-vous d'ajuster le chemin
 export class UserProfileComponent {
 
   //Bonus POO : créer une classe User qui te permet d'instancier ton objet user grâce au mot clef new depuis ton composant user-profile.
+
+
   user = new User('Doe', 'John', 25, '', 'https://randomuser.me/api/portraits/lego/2.jpg');
 
   hideAge = false;
